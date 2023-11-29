8f0a7d7e4000c9f9a81340ae0178c893b5c743d2
Fredi Kats <fredikats@outlook.com>
2023-11-29 15:24:45 +0100

    Implement property extracting from MSBuild

---
+++ Sources/Kysect.Zeya.ManagedDotnetCli/DotnetCliException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kysect.Zeya.ManagedDotnetCli;
+
+public class DotnetCliException : Exception
+{
+    public DotnetCliException(string message) : base(message)
+    {
+    }
+
+    public DotnetCliException() : base("Failed to execute cmd command")
+    {
+    }
+
+    public DotnetCliException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ Sources/Kysect.Zeya.Tests/DotnetCliTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Kysect.CommonLib.DependencyInjection;
+using Kysect.Zeya.ManagedDotnetCli;
+
+namespace Kysect.Zeya.Tests;
+
+public class DotnetCliTests
+{
+    [Test]
+    public void GetProperty_ForManagePackageVersionsCentrally_ReturnFalse()
+    {
+        var logger = PredefinedLogger.CreateConsoleLogger();
+        var dotnetCli = new DotnetCli(logger);
+
+        var isTestProject = dotnetCli.GetProperty(Path.Combine("..", "..", "..", "Kysect.Zeya.Tests.csproj"), "IsTestProject");
+
+        var parsed = bool.TryParse(isTestProject, out var result);
+        parsed.Should().BeTrue();
+        result.Should().BeTrue();
+    }
+}
