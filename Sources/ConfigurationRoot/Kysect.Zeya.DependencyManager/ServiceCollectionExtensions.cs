fcad719b1e2b706becc9f48fd73f97caa869e0aa
Fredi Kats <fredikats@outlook.com>
2023-12-09 09:21:11 +0100

    Introduce project for dependencies configuration

---
+++ Sources/ConfigurationRoot/Kysect.Zeya.DependencyManager/ServiceCollectionExtensions.cs
@@ -0,0 +1,112 @@
+﻿using Kysect.CommonLib.DependencyInjection;
+using Kysect.CommonLib.DependencyInjection.Logging;
+using Kysect.GithubUtils.RepositorySync;
+using Kysect.Zeya.Abstractions.Contracts;
+using Kysect.Zeya.GithubIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Octokit;
+using System.IO.Abstractions;
+using System.Reflection;
+using Kysect.ScenarioLib;
+using Kysect.ScenarioLib.Abstractions;
+using Kysect.ScenarioLib.YamlParser;
+using Kysect.Zeya.ValidationRules;
+using Kysect.Zeya.ManagedDotnetCli;
+using Kysect.Zeya.ProjectSystemIntegration;
+using Kysect.Zeya.RepositoryValidation;
+
+namespace Kysect.Zeya.DependencyManager;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddZeyaRequiredService(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddZeyaConfiguration()
+            .AddZeyaLogging()
+            .AddZeyaGithubIntegration()
+            .AddZeyaRepositoryValidation();
+    }
+
+    public static IServiceCollection AddZeyaConfiguration(this IServiceCollection serviceCollection)
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return serviceCollection
+            .AddSingleton(config)
+            .AddOptionsWithValidation<GithubIntegrationOptions>("GithubIntegrationOptions");
+    }
+
+    public static IServiceCollection AddZeyaLogging(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection.AddSingleton(_ => CreateLogger());
+    }
+
+    public static IServiceCollection AddZeyaGithubIntegration(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IPathFormatStrategy>(sp =>
+        {
+            var githubIntegrationOptions = sp.GetRequiredService<IOptions<GithubIntegrationOptions>>();
+            return new UseOwnerAndRepoForFolderNameStrategy(githubIntegrationOptions.Value.CacheDirectoryPath);
+        });
+
+        serviceCollection.AddSingleton<IGitHubClient>(sp =>
+        {
+            var githubIntegrationOptions = sp.GetRequiredService<IOptions<GithubIntegrationOptions>>();
+            return new GitHubClient(new ProductHeaderValue("Zeya")) { Credentials = new Credentials(githubIntegrationOptions.Value.GithubToken) };
+        });
+
+        return serviceCollection
+            .AddSingleton<IFileSystem, FileSystem>()
+            .AddSingleton<IGithubRepositoryProvider, GithubRepositoryProvider>()
+            .AddSingleton<IGithubIntegrationService, GithubIntegrationService>();
+    }
+
+    public static IServiceCollection AddZeyaRepositoryValidation(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<DotnetCli>();
+        serviceCollection.AddSingleton<DirectoryPackagesParser>();
+        serviceCollection.AddSingleton<IRepositoryValidationReporter, LoggerRepositoryValidationReporter>();
+        serviceCollection.AddSingleton<RepositoryValidator>();
+        serviceCollection = serviceCollection.AddZeyaScenarioExecution();
+
+        return serviceCollection;
+    }
+
+    // TODO: disable IncludeScopes in default implementation PredefinedLogger.CreateConsoleLogger
+    private static ILogger CreateLogger(LogLevel logLevel = LogLevel.Trace)
+    {
+        using var logConfigurationBuilder = new LogConfigurationBuilder();
+
+        return logConfigurationBuilder
+            .SetLevel(logLevel)
+            .SetDefaultCategory("Zeya")
+            .AddSpectreConsole()
+            .AddSerilogToFile("Zeya.log")
+            .Build();
+    }
+
+    private static IServiceCollection AddZeyaScenarioExecution(this IServiceCollection serviceCollection)
+    {
+        Assembly[] assemblies = new[]
+        {
+            // Definition of assembly with IScenarioStep implementation
+            // Definition of assembly with IScenarioStepExecutor implementation
+            typeof(RepositoryValidationContext).Assembly
+        };
+
+        var dummyScenarioSourceProvider = new ScenarioSourceProvider("Samples");
+
+        return serviceCollection
+            .AddSingleton<IScenarioSourceProvider>(sp => dummyScenarioSourceProvider)
+            .AddSingleton<IScenarioSourceCodeParser, YamlScenarioSourceCodeParser>()
+            .AddSingleton<IScenarioStepParser, ScenarioStepReflectionParser>(_ => ScenarioStepReflectionParser.Create(assemblies))
+            .AddAllImplementationOf<IScenarioStepExecutor>(assemblies)
+            .AddSingleton<IScenarioStepHandler, ScenarioStepReflectionHandler>(sp => ScenarioStepReflectionHandler.Create(sp, assemblies));
+    }
+}
