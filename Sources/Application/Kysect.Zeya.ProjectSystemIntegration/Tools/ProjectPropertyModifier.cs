ac0e899aaeef0c05e7c02d65c61853fa73776c06
Fredi Kats <fredikats@outlook.com>
2023-12-10 14:51:58 +0100

    Implement fixer for target version validation rule

---
+++ Sources/Application/Kysect.Zeya.ProjectSystemIntegration/Tools/ProjectPropertyModifier.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using Kysect.CommonLib.Collections.Extensions;
+using Kysect.DotnetSlnParser.Parsers;
+using Kysect.DotnetSlnParser.Tools;
+using Kysect.Zeya.Abstractions;
+using Kysect.Zeya.ProjectSystemIntegration.XmlProjectFileModifyStrategies;
+using Microsoft.Extensions.Logging;
+using Microsoft.Language.Xml;
+
+namespace Kysect.Zeya.ProjectSystemIntegration.Tools;
+
+public class ProjectPropertyModifier(XmlProjectFileAccessor projectFileAccessor, ILogger logger)
+{
+    public void AddOrUpdateProperty(string key, string value)
+    {
+        if (projectFileAccessor.Document.RootSyntax is null)
+            projectFileAccessor.UpdateDocument(AddEmptyProjectNode);
+
+        projectFileAccessor.UpdateDocument(AddPropertyGroupNodeIfNotExistsModificationStrategy.Instance);
+        Modify(key, value);
+    }
+
+    private void Modify(string key, string value)
+    {
+        var propertyNodes = projectFileAccessor.GetNodesByName(key);
+        if (propertyNodes.Any())
+        {
+            if (propertyNodes.Count > 1)
+                logger.LogWarning("Found multiple nodes with name {Name}", key);
+
+            projectFileAccessor.UpdateDocument(d =>
+            {
+                foreach (IXmlElementSyntax elementSyntax in propertyNodes)
+                {
+                    IXmlElementSyntax modifiedPropertyElement = elementSyntax.WithContent(SyntaxFactory.List(ExtendedSyntaxFactory.XmlName(value)));
+                    d = d.ReplaceNode(elementSyntax.AsNode, modifiedPropertyElement.AsNode);
+                }
+
+                return d;
+            });
+
+            return;
+        }
+
+        IXmlElementSyntax propertyGroup = SelectPropertyGroupForInsert();
+        var modifiedPropertyGroup = propertyGroup.AddChild(ExtendedSyntaxFactory.PropertyGroupParameter(key, value));
+        projectFileAccessor.UpdateDocument(d => d.ReplaceNode(propertyGroup.AsNode, modifiedPropertyGroup.AsNode));
+    }
+
+    private IXmlElementSyntax SelectPropertyGroupForInsert()
+    {
+        // TODO: introduce constant PropertyGroup
+        var propertyGroupNodes = projectFileAccessor.Document.GetNodesByName("PropertyGroup");
+        if (propertyGroupNodes.IsEmpty())
+            projectFileAccessor.UpdateDocument(AddPropertyGroupNodeIfNotExistsModificationStrategy.Instance);
+
+        propertyGroupNodes = projectFileAccessor.Document.GetNodesByName("PropertyGroup");
+        if (propertyGroupNodes.IsEmpty())
+            throw new ZeyaException("Unexpected missed PropertyGroup node");
+
+        return propertyGroupNodes.First();
+    }
+
+    private XmlDocumentSyntax AddEmptyProjectNode(XmlDocumentSyntax syntax)
+    {
+        var contentTemplate = """
+                              <Project>
+                              </Project>
+                              """;
+
+        return Parser.ParseText(contentTemplate);
+    }
+}
---
+++ Sources/Tests/Kysect.Zeya.Tests/ValidationRuleFixers/TargetFrameworkVersionAllowedValidationRuleFixerTests.cs
@@ -0,0 +1,63 @@
+﻿using Kysect.Zeya.ValidationRules.Fixers.SourceCode;
+using System.IO.Abstractions.TestingHelpers;
+using Kysect.Zeya.ValidationRules.Rules.SourceCode;
+using Microsoft.Extensions.Logging;
+using FluentAssertions;
+using Kysect.DotnetSlnGenerator;
+using Kysect.Zeya.Abstractions.Models;
+using Kysect.Zeya.GithubIntegration;
+using Kysect.Zeya.Tests.Tools;
+using Kysect.CommonLib.DependencyInjection.Logging;
+
+namespace Kysect.Zeya.Tests.ValidationRuleFixers;
+
+public class TargetFrameworkVersionAllowedValidationRuleFixerTests
+{
+    private FakeDotnetProjectPropertyAccessor _projectPropertyAccessor;
+    private TargetFrameworkVersionAllowedValidationRuleFixer _fixer;
+    private MockFileSystem _fileSystem;
+    private ILogger _logger;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = DefaultLoggerConfiguration.CreateConsoleLogger();
+        _fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+        _projectPropertyAccessor = new FakeDotnetProjectPropertyAccessor();
+        _fixer = new TargetFrameworkVersionAllowedValidationRuleFixer(_fileSystem, _projectPropertyAccessor, _logger);
+    }
+
+    [Test]
+    public void Fix_Net6_ChangedToNet8()
+    {
+        var originalProjectContent = """
+                                     <Project Sdk="Microsoft.NET.Sdk">
+                                       <PropertyGroup>
+                                         <TargetFramework>net6.0</TargetFramework>
+                                       </PropertyGroup>
+                                     </Project>
+                                     """;
+
+        var expectedProjectContent = """
+                                     <Project Sdk="Microsoft.NET.Sdk">
+                                       <PropertyGroup>
+                                         <TargetFramework>net8.0</TargetFramework>
+                                       </PropertyGroup>
+                                     </Project>
+                                     """;
+
+        // TODO: this is hack because target framework version is fetched from dotnet CLI. Need to rework in future
+        _projectPropertyAccessor.TargetFramework = "net6.0";
+
+        string currentPath = _fileSystem.Path.GetFullPath(".");
+        const string projectName = "SampleProject";
+        var solutionBuilder = new DotnetSolutionBuilder("Solution")
+            .AddProject(new DotnetProjectBuilder(projectName, originalProjectContent));
+        solutionBuilder.Save(_fileSystem, currentPath);
+
+        var repositoryAccessor = new GithubRepositoryAccessor(new GithubRepository("owner", "name"), new FakePathFormatStrategy(currentPath), _fileSystem);
+        _fixer.Fix(new TargetFrameworkVersionAllowedValidationRule.Arguments(["net8.0"]), repositoryAccessor);
+
+        _fileSystem.File.ReadAllText(_fileSystem.Path.Combine(currentPath, projectName, $"{projectName}.csproj")).Should().Be(expectedProjectContent);
+    }
+}
