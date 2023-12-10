b51fc5a4bd99ba36246d4643a3ef1ac551042f8e
Fredi Kats <fredikats@outlook.com>
2023-12-10 16:08:21 +0100

    Inctroduce IXmlDocumentModificationStrategy

--- Sources/Application/Kysect.Zeya.ProjectSystemIntegration/Tools/ProjectPropertyModifier.cs
+++ Sources/Application/Kysect.Zeya.ProjectSystemIntegration/Tools/ProjectPropertyModifier.cs
@@ -3,32 +3,40 @@ using Kysect.CommonLib.Collections.Extensions;
 using Kysect.DotnetSlnParser.Parsers;
 using Kysect.DotnetSlnParser.Tools;
 using Kysect.Zeya.Abstractions;
+using Kysect.Zeya.ProjectSystemIntegration.XmlDocumentModificationStrategies;
 using Kysect.Zeya.ProjectSystemIntegration.XmlProjectFileModifyStrategies;
 using Microsoft.Extensions.Logging;
 using Microsoft.Language.Xml;
 
 namespace Kysect.Zeya.ProjectSystemIntegration.Tools;
 
-public class ProjectPropertyModifier(XmlProjectFileAccessor projectFileAccessor, ILogger logger)
+public class ProjectPropertyModifier
 {
+    private readonly XmlProjectFileAccessor _projectFileAccessor;
+    private readonly ILogger _logger;
+
+    public ProjectPropertyModifier(XmlProjectFileAccessor projectFileAccessor, ILogger logger)
+    {
+        _projectFileAccessor = projectFileAccessor;
+        _logger = logger;
+
+        projectFileAccessor.UpdateDocument(CreateProjectDocumentIfEmptyModificationStrategy.Instance);
+    }
+
     public void AddOrUpdateProperty(string key, string value)
     {
-        if (projectFileAccessor.Document.RootSyntax is null)
-            projectFileAccessor.UpdateDocument(AddEmptyProjectNode);
-
-        projectFileAccessor.UpdateDocument(AddPropertyGroupNodeIfNotExistsModificationStrategy.Instance);
         Modify(key, value);
     }
 
     private void Modify(string key, string value)
     {
-        var propertyNodes = projectFileAccessor.GetNodesByName(key);
+        var propertyNodes = _projectFileAccessor.GetNodesByName(key);
         if (propertyNodes.Any())
         {
             if (propertyNodes.Count > 1)
-                logger.LogWarning("Found multiple nodes with name {Name}", key);
+                _logger.LogWarning("Found multiple nodes with name {Name}", key);
 
-            projectFileAccessor.UpdateDocument(d =>
+            _projectFileAccessor.UpdateDocument(d =>
             {
                 foreach (IXmlElementSyntax elementSyntax in propertyNodes)
                 {
@@ -44,30 +52,20 @@ public class ProjectPropertyModifier(XmlProjectFileAccessor projectFileAccessor,
 
         IXmlElementSyntax propertyGroup = SelectPropertyGroupForInsert();
         var modifiedPropertyGroup = propertyGroup.AddChild(ExtendedSyntaxFactory.PropertyGroupParameter(key, value));
-        projectFileAccessor.UpdateDocument(d => d.ReplaceNode(propertyGroup.AsNode, modifiedPropertyGroup.AsNode));
+        _projectFileAccessor.UpdateDocument(d => d.ReplaceNode(propertyGroup.AsNode, modifiedPropertyGroup.AsNode));
     }
 
     private IXmlElementSyntax SelectPropertyGroupForInsert()
     {
         // TODO: introduce constant PropertyGroup
-        var propertyGroupNodes = projectFileAccessor.Document.GetNodesByName("PropertyGroup");
+        var propertyGroupNodes = _projectFileAccessor.Document.GetNodesByName("PropertyGroup");
         if (propertyGroupNodes.IsEmpty())
-            projectFileAccessor.UpdateDocument(AddPropertyGroupNodeIfNotExistsModificationStrategy.Instance);
+            _projectFileAccessor.UpdateDocument(AddPropertyGroupNodeIfNotExistsModificationStrategy.Instance);
 
-        propertyGroupNodes = projectFileAccessor.Document.GetNodesByName("PropertyGroup");
+        propertyGroupNodes = _projectFileAccessor.Document.GetNodesByName("PropertyGroup");
         if (propertyGroupNodes.IsEmpty())
             throw new ZeyaException("Unexpected missed PropertyGroup node");
 
         return propertyGroupNodes.First();
-    }
-
-    private XmlDocumentSyntax AddEmptyProjectNode(XmlDocumentSyntax syntax)
-    {
-        var contentTemplate = """
-                              <Project>
-                              </Project>
-                              """;
-
-        return Parser.ParseText(contentTemplate);
     }
 }
