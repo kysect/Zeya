c90e6faf365140c53cf26684bbe4a0527be193c0
Fredi Kats <fredikats@outlook.com>
2023-12-10 17:09:20 +0100

    Implement fixer for CPM synchronization

---
+++ Sources/Application/Kysect.Zeya.ProjectSystemIntegration/Tools/XmlProjectFileAccessorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Kysect.DotnetSlnParser.Parsers;
+
+namespace Kysect.Zeya.ProjectSystemIntegration.Tools;
+
+public static class XmlProjectFileAccessorExtensions
+{
+    public static bool IsEmpty(this XmlProjectFileAccessor document)
+    {
+        return document.Document.RootSyntax is not null;
+    }
+}
