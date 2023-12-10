2ad6457162c8dac81c15ad37eacd4cdb42931945
Fredi Kats <fredikats@outlook.com>
2023-12-10 18:12:18 +0100

    Implement fixer for required nuget metadata properties

---
+++ Sources/Application/Kysect.Zeya.ValidationRules/Fixers/Nuget/NugetMetadataHaveCorrectValueValidationRuleFixer.cs
@@ -0,0 +1,25 @@
+﻿using Kysect.Zeya.Abstractions.Contracts;
+using Kysect.Zeya.ProjectSystemIntegration;
+using Kysect.Zeya.ProjectSystemIntegration.Tools;
+using Kysect.Zeya.ValidationRules.Rules.Nuget;
+using Microsoft.Extensions.Logging;
+
+namespace Kysect.Zeya.ValidationRules.Fixers.Nuget;
+
+public class NugetMetadataHaveCorrectValueValidationRuleFixer(DotnetSolutionModifierFactory dotnetSolutionModifierFactory, ILogger logger) : IValidationRuleFixer<NugetMetadataHaveCorrectValueValidationRule.Arguments>
+{
+    public void Fix(NugetMetadataHaveCorrectValueValidationRule.Arguments rule, IGithubRepositoryAccessor githubRepository)
+    {
+        var solutionPath = githubRepository.GetSolutionFilePath();
+        var solutionModifier = dotnetSolutionModifierFactory.Create(solutionPath);
+
+        logger.LogTrace("Apply changes to {FileName} file", ValidationConstants.DirectoryBuildPropsFileName);
+
+        var projectPropertyModifier = new ProjectPropertyModifier(solutionModifier.DirectoryBuildPropsModifier.Accessor, logger);
+        foreach (var (key, value) in rule.RequiredKeyValues)
+            projectPropertyModifier.AddOrUpdateProperty(key, value);
+        
+        // TODO: force somehow saving
+        solutionModifier.Save();
+    }
+}
