6957ea1cbc843a6b6a4f37ce178b8332ef1e4bdb
Fredi Kats <fredikats@outlook.com>
2023-12-10 16:08:21 +0100

    Inctroduce IXmlDocumentModificationStrategy

---
+++ Sources/Application/Kysect.Zeya.ProjectSystemIntegration/XmlDocumentModificationStrategies/IXmlDocumentModificationStrategy.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Language.Xml;
+
+namespace Kysect.Zeya.ProjectSystemIntegration.XmlDocumentModificationStrategies;
+
+public interface IXmlDocumentModificationStrategy
+{
+    XmlDocumentSyntax Modify(XmlDocumentSyntax document);
+}
