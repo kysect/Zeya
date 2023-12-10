535efef08716a116f54c88e16a16432bd62db134
Fredi Kats <fredikats@outlook.com>
2023-12-10 17:09:20 +0100

    Implement fixer for CPM synchronization

---
+++ Sources/Application/Kysect.Zeya.ProjectSystemIntegration/XmlProjectFileModifyStrategies/SyncCentralPackageManagementVersionsModificationStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kysect.DotnetSlnParser.Modifiers;
+using Kysect.DotnetSlnParser.Tools;
+using Kysect.Zeya.ProjectSystemIntegration.Tools;
+using Microsoft.Language.Xml;
+
+namespace Kysect.Zeya.ProjectSystemIntegration.XmlProjectFileModifyStrategies;
+
+public class SyncCentralPackageManagementVersionsModificationStrategy(Dictionary<string, string> versions) : IXmlProjectFileModifyStrategy<XmlEmptyElementSyntax>
+{
+    public IReadOnlyCollection<XmlEmptyElementSyntax> SelectNodeForModify(XmlDocumentSyntax document)
+    {
+        var specifiedPackages = versions.Keys.ToHashSet();
+
+        var packageNodes = document
+            .GetNodesByName("PackageVersion")
+            .Where(n => n.Attributes.Any(a => specifiedPackages.Contains(a.Value)))
+            .OfType<XmlEmptyElementSyntax>()
+            .ToList();
+
+        return packageNodes;
+    }
+
+    public SyntaxNode ApplyChanges(XmlEmptyElementSyntax syntax)
+    {
+        var packageName = syntax.GetAttribute("Include").Value;
+
+        var oldVersionAttribute = syntax.GetAttribute("Version");
+
+        return syntax.ReplaceNode(oldVersionAttribute, ExtendedSyntaxFactory.XmlAttribute("Version", versions[packageName]));
+    }
+}
---
+++ Sources/Application/Kysect.Zeya.ValidationRules/Fixers/SourceCode/NugetVersionSynchronizedWithMasterCentralPackageManagerValidationRuleFixer.cs
@@ -0,0 +1,44 @@
+﻿using Kysect.Zeya.Abstractions.Contracts;
+using Kysect.Zeya.ProjectSystemIntegration;
+using Kysect.Zeya.ValidationRules.Rules.SourceCode;
+using Microsoft.Extensions.Logging;
+using System.IO.Abstractions;
+using Kysect.Zeya.ProjectSystemIntegration.Tools;
+using Kysect.Zeya.ProjectSystemIntegration.XmlProjectFileModifyStrategies;
+
+namespace Kysect.Zeya.ValidationRules.Fixers.SourceCode;
+
+public class NugetVersionSynchronizedWithMasterCentralPackageManagerValidationRuleFixer(
+    DotnetSolutionModifierFactory dotnetSolutionModifierFactory,
+    IFileSystem fileSystem,
+    DirectoryPackagesParser directoryPackagesParser,
+    ILogger logger)
+    : IValidationRuleFixer<NugetVersionSynchronizedWithMasterCentralPackageManagerValidationRule.Arguments>
+{
+    public void Fix(NugetVersionSynchronizedWithMasterCentralPackageManagerValidationRule.Arguments rule, IGithubRepositoryAccessor githubRepository)
+    {
+        var solutionPath = githubRepository.GetSolutionFilePath();
+        var solutionModifier = dotnetSolutionModifierFactory.Create(solutionPath);
+
+        if (solutionModifier.DirectoryPackagePropsModifier.Accessor.IsEmpty())
+        {
+            logger.LogWarning("Project is not use CPM, skip version fixer");
+            return;
+        }
+
+        if (!fileSystem.File.Exists(rule.MasterFile))
+        {
+            // TODO: after this error validation should finish as failed
+            logger.LogError("Master file {File} for checking CPM was not found.", rule.MasterFile);
+            return;
+        }
+
+        var masterFileContent = fileSystem.File.ReadAllText(rule.MasterFile);
+        var masterPackages = directoryPackagesParser
+            .Parse(masterFileContent)
+            .ToDictionary(p => p.PackageName, p => p.Version);
+        
+        solutionModifier.DirectoryPackagePropsModifier.Accessor.UpdateDocument(new SyncCentralPackageManagementVersionsModificationStrategy(masterPackages));
+        solutionModifier.Save();
+    }
+}
