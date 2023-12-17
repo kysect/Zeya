c91bea9c1379fc3513abe705fa5043cf538771ce
Fredi Kats <fredikats@outlook.com>
2023-12-17 22:20:22 +0100

    Add parent directory creation before copy github workflow to directory

--- Sources/Application/Kysect.Zeya.GithubIntegration/GithubRepositoryAccessor.cs
+++ Sources/Application/Kysect.Zeya.GithubIntegration/GithubRepositoryAccessor.cs
@@ -1,4 +1,5 @@
-﻿using Kysect.GithubUtils.RepositorySync;
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.GithubUtils.RepositorySync;
 using Kysect.Zeya.Abstractions;
 using Kysect.Zeya.Abstractions.Contracts;
 using Kysect.Zeya.Abstractions.Models;
@@ -29,7 +30,22 @@ public class GithubRepositoryAccessor(GithubRepository repository, IPathFormatSt
     public void WriteAllText(string partialPath, string content)
     {
         string fullPathToFile = GetFullPathToFile(partialPath);
+        var fileInfo = fileSystem.FileInfo.New(fullPathToFile);
+        EnsureContainingDirectoryExists(fileSystem, fileInfo);
         fileSystem.File.WriteAllText(fullPathToFile, content);
+    }
+
+    // TODO: move to common lib
+    private static void EnsureContainingDirectoryExists(IFileSystem fileSystem, IFileInfo fileInfo)
+    {
+        fileSystem.ThrowIfNull();
+        fileInfo.ThrowIfNull();
+
+        if (fileInfo.Directory == null)
+            throw new ArgumentException($"Cannot get directory for path {fileInfo.FullName}");
+
+        if (!fileSystem.Directory.Exists(fileInfo.Directory.FullName))
+            fileSystem.Directory.CreateDirectory(fileInfo.Directory.FullName);
     }
 
     public string GetWorkflowPath(string workflowName)
