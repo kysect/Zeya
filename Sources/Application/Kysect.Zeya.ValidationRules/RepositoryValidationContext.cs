42b5292aa81bf3b93325951a337cf6d45cc32c0a
Fredi Kats <fredikats@outlook.com>
2023-12-25 12:02:39 +0100

    Implement RepositoryValidationContext creation without GitHub

--- Sources/Application/Kysect.Zeya.ValidationRules/RepositoryValidationContext.cs
+++ Sources/Application/Kysect.Zeya.ValidationRules/RepositoryValidationContext.cs
@@ -3,4 +3,33 @@ using Kysect.Zeya.Abstractions.Models;
 
 namespace Kysect.Zeya.ValidationRules;
 
-public record RepositoryValidationContext(GithubRepository GithubMetadata, IClonedRepository Repository, RepositoryDiagnosticCollector DiagnosticCollector);
+public class RepositoryValidationContext
+{
+    public static RepositoryValidationContext CreateForGitHub(GithubRepository githubMetadata, IClonedRepository repository, RepositoryDiagnosticCollector diagnosticCollector)
+    {
+        return new RepositoryValidationContext(githubMetadata, repository, diagnosticCollector);
+    }
+
+    public static RepositoryValidationContext Create(IClonedRepository repository, RepositoryDiagnosticCollector diagnosticCollector)
+    {
+        return new RepositoryValidationContext(null, repository, diagnosticCollector);
+    }
+
+    private readonly GithubRepository? _githubRepository;
+
+    public IClonedRepository Repository { get; init; }
+    public RepositoryDiagnosticCollector DiagnosticCollector { get; init; }
+
+    public RepositoryValidationContext(GithubRepository? githubMetadata, IClonedRepository repository, RepositoryDiagnosticCollector diagnosticCollector)
+    {
+        Repository = repository;
+        DiagnosticCollector = diagnosticCollector;
+
+        _githubRepository = githubMetadata;
+    }
+
+    public GithubRepository? TryGetGithubMetadata()
+    {
+        return _githubRepository;
+    }
+}
