5be86eb12ac43198321f7282e2b673db2eb6dac1
Fredi Kats <fredikats@outlook.com>
2023-12-26 22:11:10 +0100

    Remove hardcoded path to source directory

--- Sources/Application/Kysect.Zeya.ValidationRules/ValidationConstants.cs
+++ Sources/Application/Kysect.Zeya.ValidationRules/ValidationConstants.cs
@@ -5,7 +5,6 @@ public static class ValidationConstants
     public const string LicenseFileName = "LICENSE";
     public const string ReadmeFileName = "Readme.md";
     public const string DefaultBranch = "master";
-    public const string DefaultSourceCodeDirectory = "Sources";
     public const string DirectoryPackagePropsFileName = "Directory.Packages.props";
     public const string DirectoryBuildPropsFileName = "Directory.Build.props";
 }
