f93918c039a3c8847d2b3e1b51446851f80215c6
Fredi Kats <fredikats@outlook.com>
2024-02-01 23:00:41 +0100

    Remove duplication code for reading repository in TUI

---
+++ Sources/Presentation/Kysect.Zeya.Tui/Controls/RepositoryInputControl.cs
@@ -0,0 +1,17 @@
+﻿using Kysect.Zeya.Abstractions.Models;
+using Spectre.Console;
+
+namespace Kysect.Zeya.Tui.Controls;
+
+public static class RepositoryInputControl
+{
+    public static GithubRepository Ask()
+    {
+        string repositoryFullName = AnsiConsole.Ask<string>("Repository (format: org/repo):");
+        if (!repositoryFullName.Contains('/'))
+            throw new ArgumentException("Incorrect repository format");
+
+        string[] parts = repositoryFullName.Split('/', 2);
+        return new GithubRepository(parts[0], parts[1]);
+    }
+}
