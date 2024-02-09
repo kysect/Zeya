5f6ca54c97dc026bdfcaeb25e3cc6700ea2c149c
Fredi Kats <fredikats@outlook.com>
2024-02-09 22:04:09 +0100

    Remove reference from github integration contracts to RepositoryAccess

--- Sources/Application/Kysect.Zeya.GithubIntegration.Abstraction/ClonedGithubRepositoryAccessor.cs
+++ Sources/Application/Kysect.Zeya.GithubIntegration.Abstraction/ClonedGithubRepositoryAccessor.cs
@@ -1,10 +1,43 @@
-﻿using Kysect.Zeya.GithubIntegration.Abstraction.Models;
-using Kysect.Zeya.RepositoryAccess;
+﻿using Kysect.CommonLib.FileSystem;
+using Kysect.Zeya.GithubIntegration.Abstraction.Models;
+using Kysect.Zeya.GitIntegration.Abstraction;
 using System.IO.Abstractions;
 
 namespace Kysect.Zeya.GithubIntegration.Abstraction;
 
-public class ClonedGithubRepositoryAccessor(GithubRepository githubMetadata, string repositoryRootPath, IFileSystem fileSystem) : ClonedRepositoryAccessor(repositoryRootPath, fileSystem)
+public class ClonedGithubRepositoryAccessor(GithubRepository githubMetadata, string repositoryRootPath, IFileSystem fileSystem) : IClonedRepository
 {
     public GithubRepository GithubMetadata { get; } = githubMetadata;
+
+    public string GetFullPath()
+    {
+        return repositoryRootPath;
+    }
+
+    public bool Exists(string partialPath)
+    {
+        return fileSystem.File.Exists(GetFullPathToFile(partialPath));
+    }
+
+    public string ReadAllText(string partialPath)
+    {
+        return fileSystem.File.ReadAllText(GetFullPathToFile(partialPath));
+    }
+
+    public void WriteAllText(string partialPath, string content)
+    {
+        string fullPathToFile = GetFullPathToFile(partialPath);
+        DirectoryExtensions.EnsureParentDirectoryExists(fileSystem, fullPathToFile);
+        fileSystem.File.WriteAllText(fullPathToFile, content);
+    }
+
+    public string GetWorkflowPath(string workflowName)
+    {
+        return fileSystem.Path.Combine(".github", "workflows", workflowName);
+    }
+
+    private string GetFullPathToFile(string partialPath)
+    {
+        return fileSystem.Path.Combine(GetFullPath(), partialPath);
+    }
 }
