3a4efd9e8c72429a988de32c848fd77bdacce37b
Fredi Kats <fredikats@outlook.com>
2024-02-13 22:53:41 +0100

    Add method for creating and getting ValidationPolicies from DB

---
+++ Sources/Tests/Kysect.Zeya.Tests/Tools/ZeyaDbContextProvider.cs
@@ -0,0 +1,20 @@
+﻿using Kysect.Zeya.DataAccess.EntityFramework;
+using Kysect.Zeya.DependencyManager;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kysect.Zeya.Tests.Tools;
+
+public static class ZeyaDbContextProvider
+{
+    public static IDbContextFactory<ZeyaDbContext> Create()
+    {
+        var serviceProviderOptions = new ServiceProviderOptions() { ValidateOnBuild = true, ValidateScopes = true };
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection
+            .AddZeyaDbContext();
+
+        IDbContextFactory<ZeyaDbContext> dbContextFactory = serviceCollection.BuildServiceProvider().GetRequiredService<IDbContextFactory<ZeyaDbContext>>();
+        return dbContextFactory;
+    }
+}
