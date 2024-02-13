e2679bb632c380fb24b8fb0ab91e6eee2693aab1
Fredi Kats <fredikats@outlook.com>
2024-02-13 22:53:10 +0100

    Add Entity Framework integration

---
+++ Sources/Integration/Kysect.Zeya.DataAccess.EntityFramework/ZeyaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Kysect.Zeya.DataAccess.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kysect.Zeya.DataAccess.EntityFramework;
+
+public class ZeyaDbContext : DbContext
+{
+    public DbSet<ValidationPolicyEntity> ValidationPolicies { get; set; } = null!;
+
+    public ZeyaDbContext(DbContextOptions<ZeyaDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
