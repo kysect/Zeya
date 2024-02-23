fbc8d90a4f081d8713eb2e38f715dab9910ab7c0
Fredi Kats <fredikats@outlook.com>
2024-02-23 10:48:30 +0100

    Add base infrastructure for Aspire

---
+++ Sources/ConfigurationRoot/Kysect.Zeya.WebService/Program.cs
@@ -0,0 +1,19 @@
+using Kysect.Zeya.ServiceDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
+// Add services to the container.
+builder.Services.AddProblemDetails();
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.MapDefaultEndpoints();
+app.Run();
