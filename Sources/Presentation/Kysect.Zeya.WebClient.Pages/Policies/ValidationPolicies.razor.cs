93d9feb8b7ff501a97e89bed4ec306b265c40e91
Fredi Kats <fredikats@outlook.com>
2024-02-28 08:20:05 +0100

    Add page for validation policies

---
+++ Sources/Presentation/Kysect.Zeya.WebClient.Pages/Policies/ValidationPolicies.razor.cs
@@ -0,0 +1,26 @@
+﻿using Kysect.Zeya.Client.Abstractions.Contracts;
+using Kysect.Zeya.Client.Abstractions.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kysect.Zeya.WebClient.Pages.Policies;
+
+public partial class ValidationPolicies
+{
+    [Inject] private NavigationManager _navigationManager { get; set; } = null!;
+    [Inject] private IValidationPolicyApi _validationPolicyApi { get; set; } = null!;
+
+    private IReadOnlyCollection<ValidationPolicyDto>? _validationPolicies;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _validationPolicies = await _validationPolicyApi.ReadPolicies();
+    }
+
+    private void EditPolicy(Guid id)
+    {
+        _navigationManager.NavigateTo($"validation-policies/{id}");
+    }
+}
