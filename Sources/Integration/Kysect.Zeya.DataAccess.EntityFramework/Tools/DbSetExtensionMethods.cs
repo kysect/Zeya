240f7e25a1f479019a84926d1f913898180b8de4
Fredi Kats <fredikats@outlook.com>
2024-03-20 23:01:44 +0100

    Add extension method for getting entities from DbSet with null check

---
+++ Sources/Integration/Kysect.Zeya.DataAccess.EntityFramework/Tools/DbSetExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.CommonLib.Collections.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kysect.Zeya.DataAccess.EntityFramework.Tools;
+
+public static class DbSetExtensionMethods
+{
+    public static async ValueTask<T> GetAsync<T>(this DbSet<T> dbSet, params object?[] keyValues) where T : class
+    {
+        dbSet.ThrowIfNull();
+        keyValues.ThrowIfNull();
+
+        T? result = await dbSet.FindAsync();
+        return result ?? throw new ArgumentException($"Cannot find {typeof(T)} by key {keyValues.ToSingleString()}");
+    }
+}
