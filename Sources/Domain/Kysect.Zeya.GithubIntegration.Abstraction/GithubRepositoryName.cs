e2ef05d14bf701834a2b65bed2d2ce2583b0c0df
Fredi Kats <fredikats@outlook.com>
2024-03-21 18:47:13 +0100

    Move github naming parsing to common project

--- Sources/Domain/Kysect.Zeya.GithubIntegration.Abstraction/GithubRepositoryName.cs
+++ Sources/Domain/Kysect.Zeya.GithubIntegration.Abstraction/GithubRepositoryName.cs
@@ -1,6 +1,21 @@
-﻿namespace Kysect.Zeya.GithubIntegration.Abstraction;
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.Zeya.Common;
+
+namespace Kysect.Zeya.GithubIntegration.Abstraction;
 
 public record struct GithubRepositoryName(string Owner, string Name)
 {
+    public static GithubRepositoryName Parse(string value)
+    {
+        value.ThrowIfNull();
+
+        if (!value.Contains('/'))
+            throw new ZeyaException("Repository does not contains '/'");
+
+        string[] parts = value.Split('/', 2);
+
+        return new GithubRepositoryName(parts[0], parts[1]);
+    }
+
     public readonly string FullName => $"{Owner}/{Name}";
 }
