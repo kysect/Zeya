0cec11002a831ccd24f6b505521b9c255b289fe9
Fredi Kats <fredikats@outlook.com>
2024-04-04 10:06:38 +0200

    Add logic for getting git diff from repository

---
+++ Sources/Tests/Kysect.Zeya.Tests/Tools/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+
+namespace Kysect.Zeya.Tests.Tools;
+
+public static class StringExtensions
+{
+    public static string NormalizeEndLines(this string value)
+    {
+        value.ThrowIfNull();
+
+        return value
+            .Replace("\r\n", "\n")
+            .Replace("\n", Environment.NewLine);
+    }
+}
