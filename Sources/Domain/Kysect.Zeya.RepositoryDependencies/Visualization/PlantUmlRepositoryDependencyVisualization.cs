2929f89df2ae70cda46a790d933c71cc168b72c2
Fredi Kats <fredikats@outlook.com>
2024-05-11 09:18:25 +0200

    Add color for repositories with diagnostics on dependency graph

--- Sources/Domain/Kysect.Zeya.RepositoryDependencies/Visualization/PlantUmlRepositoryDependencyVisualization.cs
+++ Sources/Domain/Kysect.Zeya.RepositoryDependencies/Visualization/PlantUmlRepositoryDependencyVisualization.cs
@@ -6,16 +6,19 @@ using System.Text;
 
 namespace Kysect.Zeya.RepositoryDependencies.Visualization;
 
+// TODO: I hope this class will be replaced with library for PlantUML
 public class PlantUmlRepositoryDependencyVisualization
 {
-    public string ConvertToString(IReadOnlyCollection<GraphLink<string>> graphLinks)
+    public string ConvertToString(IReadOnlyCollection<GraphLink<string>> graphLinks, IReadOnlyCollection<string> repositoriesWithDiagnostics)
     {
+        const string orangeColorCode = "%23F80";
         graphLinks.ThrowIfNull();
 
         var builder = new StringBuilder();
         // TODO: Investigate what to do with ';'. gravizo don't work without it.
 
         builder.AppendLine("@startuml;");
+        builder.AppendLine("skinparam monochrome false;");
 
         List<string> components = graphLinks
             .SelectMany(g => new[] { g.From, g.To })
@@ -23,7 +26,16 @@ public class PlantUmlRepositoryDependencyVisualization
             .ToList();
 
         foreach (string component in components)
-            builder.AppendLine($"component {component};");
+        {
+            if (!repositoriesWithDiagnostics.Contains(component))
+            {
+                builder.AppendLine($"component {component};");
+            }
+            else
+            {
+                builder.AppendLine($"component {component} {orangeColorCode};");
+            }
+        }
 
         foreach ((string? from, string? to) in graphLinks)
             builder.AppendLine($"{from} --> {to};");
